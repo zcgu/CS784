69e384d78a9b951eb5176bf9c2e2df01fa2f1191
Jasonsu9105 <jasonsu9105@gmail.com>
2016-02-23 23:39:19 -0600

    outlier

---
+++ stage1_4_2_Outlier.py
@@ -0,0 +1,133 @@
+import json
+import codecs
+import re
+
+#get some statistics
+def getList():
+    
+    type = u'Product Type'
+    search = u'Hard Drive Capacity'
+    
+    productType_map = {}
+    hardDrive_map = {}
+    
+    f = codecs.open('elec_pairs_stage1.txt', 'r', errors='ignore')
+    
+    for line in f:
+        line = unicode(line, errors='ignore')
+        line_split = line.split('?')
+    
+        json1 = json.loads(line_split[2])   # json for product 1
+        json2 = json.loads(line_split[4])   # json for product 2
+        
+        if type in json1:
+            type1 = json1[type][0]
+            #print type1
+            if type1 in productType_map:
+                productType_map[type1] += 1
+            else:
+                productType_map[type1] = 1
+            
+            if search in json1:
+                #print json1[search][0],'   ', type1              
+                if type1 in hardDrive_map:
+                    hardDrive_map[type1] += 1 
+                else:
+                    hardDrive_map[type1] = 1
+            
+        if type in json2:
+            type2 = json2[type][0]
+            #print type1
+            if type2 in productType_map:
+                productType_map[type2] += 1
+            else:
+                productType_map[type2] = 1
+                
+            if search in json2:
+                if type2 in hardDrive_map:
+                    hardDrive_map[type2] += 1 
+                else:
+                    hardDrive_map[type2] = 1
+                    
+                
+    sorted_list = sorted(productType_map.items(), key=lambda d: d[1], reverse=True)
+    
+    sorted_list2 = sorted(hardDrive_map.items(), key=lambda d: d[1], reverse=True)
+    
+    for tuple in sorted_list2:
+        print tuple
+    
+    return sorted_list
+    
+
+#find distribution
+def outlier():
+    
+    type = u'Product Type'
+    outlierType = u'Desktop Computers' #find outlier within same product type
+    outlierSearch = u'Hard Drive Capacity'     #search criteria
+    assembledLength_map = {}
+    
+    f = codecs.open('elec_pairs_stage1.txt', 'r', errors='ignore')
+    
+    for line in f:
+        line = unicode(line, errors='ignore')
+        line_split = line.split('?')
+    
+        json1 = json.loads(line_split[2])   # json for product 1
+        json2 = json.loads(line_split[4])   # json for product 2
+        
+        if type in json1 and outlierSearch in json1:
+            type1 = json1[type][0]
+            if type1 == outlierType:
+                value1 = json1[outlierSearch][0]
+                
+                #if value1 == '1536':
+              #      print line_split[0]
+                #value1 = int(re.sub("\D", "", value1))
+                #print value1
+                if value1 in assembledLength_map:
+                    assembledLength_map[value1] += 1
+                else:
+                    assembledLength_map[value1] = 1
+                 
+            
+        if type in json2 and outlierSearch in json2:
+                    type2 = json2[type][0]
+                    if type2 == outlierType:
+                        value2 = json2[outlierSearch][0]
+                        #if value2 == '1536':
+                         #   print line_split[0]
+                        #value2 = int(re.sub("\D", "", value2))
+                        #print value2
+                        if value2 in assembledLength_map:
+                            assembledLength_map[value2] += 1
+                        else:
+                            assembledLength_map[value2] = 1
+        else:
+            continue
+        
+    sorted_list = sorted(assembledLength_map.items(), key=lambda d: d[0])
+    
+    for k in sorted(assembledLength_map.keys()):
+        print k, '    ', assembledLength_map[k]
+    return assembledLength_map  
+
+
+    """ 
+    for tuple in sorted_list:
+        print tuple
+    
+    """
+    
+
+list = getList() 
+
+assembledLengthlist = outlier()
+
+#for k in assembledLengthlist.keys():
+  #  print k, '    ', assembledLengthlist[k]
+    
+    
+    
+    
