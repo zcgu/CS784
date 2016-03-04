36f821a49dd5b5dcc90212d4665905f7415d3134
Zhicheng <zchenggu@gmail.com>
2016-03-03 21:44:24 -0600

    put in the golden data

--- stage2/step4.py
+++ stage2/step4.py
@@ -4,3 +4,21 @@
 # YOU CAN ONLY USE THE DEVELOPMENT SET I. YOU ARE NOT SUPPOSED TO EVEN LOOK AT THE TESTING SET J.
 # We will discuss more in the class ideas on using the development set I.
 
+import codecs
+from step4_helper import *
+
+# File names.
+golden_data_file_name = 'step2_golden_data.txt'
+
+training_set_file_name = 'step2_training_set'
+tuning_set_file_name = 'step2_tuning_set'
+testing_set_file_name = 'step2_testing_set'
+
+# Read dictionary.
+dictionary = read_dictionary()
+
+# Read data into memory.
+test_data = read_data(training_set_file_name)         # Need change.
+
+# Main part.
+predict_result = []
