dfb632341a3f2a8103e5db236cbdc5b046e21a02
Zhicheng <zchenggu@gmail.com>
2016-03-04 18:20:40 -0600

    step4

--- stage2/step4.py
+++ stage2/step4.py
@@ -8,8 +8,6 @@ import codecs
 from step4_helper import *
 
 # File names.
-golden_data_file_name = 'step2_golden_data.txt'
-
 training_set_file_name = 'step2_training_set'
 tuning_set_file_name = 'step2_tuning_set'
 testing_set_file_name = 'step2_testing_set'
@@ -18,15 +16,14 @@ product_id = 'Product ID'
 product_name = 'Product Name'
 product_brand = 'Brand'
 
-# Read dictionary.
-dictionary = read_dictionary()
-
 # Read data into memory.
 test_data = read_data(training_set_file_name)
 
-# Main part.
-predict_result = []
+# Read dictionary.
+dictionary = read_dictionary()
+dictionary_set = build_dictionary_set(dictionary)
 
+# Main part.
 total_positive = 0.0
 true_positive = 0.0
 false_positive = 0.0
@@ -36,16 +33,13 @@ for item in test_data:
     item_name = item[product_name]
     item_brand = item[product_brand]
 
-    # print 'item_name: ' + item_name
-    # print 'item_brand: ' + item_brand
-    possible_brands = find_possible_brands(item_name, dictionary)
+    possible_brands = find_possible_brands(item_name, dictionary_set, dictionary)
+    possible_brands = reduce_possible_brands(item_name, possible_brands)
 
     if len(possible_brands) > 0:
-        predict_brand = possible_brands[0]
-        # print "predict_brand: " + predict_brand
+        predict_brand = select_from_possible_brands(item_name, dictionary, possible_brands)
     else:
-        predict_brand = ''
-        # print "predict_brand: Not Found!"
+        predict_brand = find_brand_not_in_dictionary(item_name)
 
     if item_brand != '':
         total_positive += 1
@@ -54,7 +48,8 @@ for item in test_data:
     if predict_brand != '' and predict_brand != item_brand:
         false_positive += 1
 
-    if item_brand != '' and predict_brand != item_brand:
+    if (item_brand != '' and predict_brand != item_brand) or \
+            (predict_brand != '' and predict_brand != item_brand):
         print 'item_name: ' + item_name
         print 'item_brand: ' + item_brand
         print 'possible_brands: ' + str(possible_brands)
--- stage2/step4_helper.py
+++ stage2/step4_helper.py
@@ -1,32 +1,34 @@
 import codecs
 import json
+import re
+import enchant
 
-dictionary_file_name = 'elec_brand_dic.txt'
+dictionary_out_name = 'step4_dictionary'
 
+common_suffix = ['inc', 'inc.', 'technology', 'technologies', 'tech', 'international',
+                 'electronic', 'electronics', 'electric']
 
 def read_dictionary():
     dictionary = []
-    f = codecs.open(dictionary_file_name, 'r', errors='ignore')
+    f = codecs.open(dictionary_out_name, 'r', errors='ignore')
     for line in f:
         line = unicode(line, errors='ignore')
 
         while line[len(line) - 1] == '\n':
             line = line[:len(line) - 1]
 
-        while line[len(line) - 1].isdigit():
-            line = line[:len(line) - 1]
-
-        while line[len(line) - 1].isspace():
-            line = line[:len(line) - 1]
-
-        if len(line) == 0:
-            print 'error'
-
         dictionary.append(line)
 
     print 'Finish reading dictionary.'
 
     return dictionary
+
+
+def build_dictionary_set(dictionary):
+    dictionary_set = set()
+    for brand in dictionary:
+        dictionary_set.add(brand)
+    return dictionary_set
 
 
 def read_data(file_name):
@@ -42,21 +44,14 @@ def read_data(file_name):
     return data
 
 
-def convert_dictionary_to_lower(dictionary):
-    lower_dictionary = []
-    for brand in dictionary:
-        lower_dictionary.append(brand.lower())
-    return lower_dictionary
-
-
-def find_possible_brands(item_name, dictionary):
-
-    lower_dictionary = convert_dictionary_to_lower(dictionary)
+def find_possible_brands(item_name, dictionary_set, dictionary):
 
     possible_brands = []
 
     item_name_split = item_name.split()
 
+    # Find every words between length 1 and 5.
+    # See if they in dictionary.
     for length in range(1, 5):
         for position in range(0, len(item_name_split) - length):
             string = item_name_split[position]
@@ -65,16 +60,133 @@ def find_possible_brands(item_name, dictionary):
                 string += ' '
                 string += item_name_split[position + i]
 
-            if string.lower() in lower_dictionary:
+            while len(string) != 0 and not string[0].isdigit() and not string[0].isalpha():
+                string = string[1:]
+            while len(string) != 0 and not string[len(string) - 1].isdigit() and not string[len(string) - 1].isalpha():
+                string = string[:len(string) - 1]
+
+            if string.lower() in dictionary_set:
+                possible_brands.append(string)
+            elif re.sub('[^0-9a-zA-Z]+', '', string.lower()) in dictionary_set:
+                dictionary_set.add(string.lower())
+                dictionary.insert(dictionary.index(re.sub('[^0-9a-zA-Z]+', '', string.lower())), string.lower())
+                possible_brands.append(string)
+            elif re.sub('[^0-9a-zA-Z]+', ' ', string.lower()) in dictionary_set:
+                dictionary_set.add(string.lower())
+                dictionary.insert(dictionary.index(re.sub('[^0-9a-zA-Z]+', ' ', string.lower())), string.lower())
                 possible_brands.append(string)
 
-    possible_brands = sorted(possible_brands, key=lambda b: lower_dictionary.index(b.lower()), reverse=False)
+    return possible_brands
 
+
+def reduce_possible_brands(item_name, possible_brands):
+
+    # If we found 'Tripp', 'Lite' and 'Tripp Lite' for for 'Tripp Lite Standard Power Cord ...'.
+    # Remove 'Tripp' and 'Lite', Keep 'Tripp Lite'.
+
+    new_possible_brands = possible_brands[:]
     for name in possible_brands:
         for other_name in possible_brands:
             if other_name != name and name in other_name:
-                possible_brands.remove(name)
+                index = other_name.find(name)
+                if index > 0:
+                    if other_name[index - 1] != ' ':
+                        continue
+                if index + len(name) < len(other_name):
+                    if other_name[index + len(name)] != ' ':
+                        continue
 
-    # print possible_brands
+                new_possible_brands.remove(name)
+                break
+    possible_brands = new_possible_brands[:]
+
+    for name in possible_brands:
+        index = item_name.find(name)
+        if index <= 0:
+            continue
+
+        prefix = item_name[:index - 1]
+        space_index = prefix.rfind(' ')
+        if space_index == -1:
+            continue
+
+        previous_word = prefix[space_index + 1:]
+
+        if previous_word.lower() == 'for':
+            possible_brands.remove(name)
 
     return possible_brands
+
+
+def select_from_possible_brands(item_name, dictionary, possible_brands):
+
+    # If any brand is at the beginning of the name, they choose it as brand.
+    # Otherwise choose the most common brand
+    for brand_name in possible_brands:
+        if item_name.find(brand_name) == 0:
+            return brand_name
+
+    for brand_name in possible_brands:
+        if item_name.find(brand_name) == item_name.find(' ') + 1:
+            return brand_name
+
+    # Convert dictionary to lower case.
+    # Sort the brand names by the frequency (The index in dictionary).
+    possible_brands = sorted(possible_brands, key=lambda b: dictionary.index(b.lower()), reverse=False)
+    return possible_brands[0]
+
+
+# See if the first few words
+def find_brand_not_in_dictionary(item_name):
+
+    item_name_split = item_name.split()
+    common_dictionary = enchant.Dict("en_US")
+
+    # Looking for 'xxx Tech ...'.
+    for string in item_name_split:
+        if item_name_split.index(string) > 4:
+            break
+        if item_name_split.index(string) == 0:
+            continue
+        if string.lower() in common_suffix:
+            return item_name[:item_name.find(string) + len(string)]
+
+    # Looking for 'DOLPHIN COMPONENTS CORP DC-8AHM Cabl'.
+    for string in item_name_split:
+        if item_name_split.index(string) > 4:
+            return item_name[:item_name.find(string) - 1]
+        if item_name_split.index(string) == 0 and not string_all_upper(string):
+            break
+        if not string_all_upper(string):
+            return item_name[:item_name.find(string) - 1]
+
+    # Looking for 'GoLight 3100 Stryker...'
+    if len(item_name_split) >= 2:
+        if not common_dictionary.check(item_name_split[0]) \
+                and not string_contains_digit(item_name_split[0]) \
+                and (string_contains_digit(item_name_split[1]) or common_dictionary.check(item_name_split[1])):
+            return item_name_split[0]
+
+    # Looking for 'Print Logic 5xxx ...'
+    if len(item_name_split) >= 3:
+        if not common_dictionary.check(item_name_split[0]) \
+                and not string_contains_digit(item_name_split[0]) \
+                and not common_dictionary.check(item_name_split[1]) \
+                and not string_contains_digit(item_name_split[1]) \
+                and (string_contains_digit(item_name_split[2]) or common_dictionary.check(item_name_split[2])):
+            return item_name[:item_name.find(item_name_split[2]) - 1]
+
+    return ''
+
+
+def string_contains_digit(string):
+    for char in string:
+        if char.isdigit():
+            return True
+    return False
+
+def string_all_upper(string):
+    for char in string:
+        if not char.isupper():
+            return False
+    return True
