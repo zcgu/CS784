b5de5b9ea590767652608c209bb7c2b3052e173f
Zhicheng <zchenggu@gmail.com>
2016-04-03 17:15:42 -0500

    stage3

--- stage3/stage3_split_data.py
+++ stage3/stage3_split_data.py
@@ -1,4 +1,5 @@
 import codecs
+import random
 
 f = codecs.open('../stage1/elec_pairs_stage1.txt', 'r', errors='ignore')
 
@@ -8,4 +9,19 @@ for line in f:
     line = unicode(line, errors='ignore')
     lines.append(line)
 
-print len(lines)
+random.seed(12345678)
+random.shuffle(lines)
+
+fw = open("X.txt", 'w')
+
+for i in range(0, len(lines) / 2):
+    fw.write(lines[i])
+
+fw.close()
+
+fw = open("Y.txt", 'w')
+
+for i in range(len(lines) / 2, len(lines)):
+    fw.write(lines[i])
+
+fw.close()
