98a827cd18464d7416d0f6a5b25c0e2ec6a07a32
Zhicheng <zchenggu@gmail.com>
2016-04-03 20:41:48 -0500

    stage3

--- stage3/stage3_generate_feature.py
+++ stage3/stage3_generate_feature.py
@@ -2,15 +2,36 @@ import stage3_helper
 from py_stringmatching import simfunctions, tokenizers
 
 
+product_type = 'Product Type'
+product_name = 'Product Name'
+product_segment = 'Product Segment'
+
+
 def generate_feature():
     lines = stage3_helper.read_file('X_training.txt')
 
-    json_label_list = []
+    jsons_label_list = []
     for line in lines:
-        json1, json2, label = stage3_helper.read_json_label_from_line(line)
-        json_label_list.append((json1, json2, label))
+        json1, json2, label = stage3_helper.read_jsons_label_from_line(line)
+        jsons_label_list.append((json1, json2, label))
 
     features = []
     labels = []
+    for jsons_label in jsons_label_list:
+        feature = []
+
+        string1, string2 = stage3_helper.get_attribute_from_jsons(jsons_label[0], jsons_label[1], product_type)
+        feature.append(simfunctions.jaccard(tokenizers.qgram(string1, 3), tokenizers.qgram(string2, 3)))
+
+        string1, string2 = stage3_helper.get_attribute_from_jsons(jsons_label[0], jsons_label[1], product_name)
+        feature.append(simfunctions.jaccard(tokenizers.qgram(string1, 3), tokenizers.qgram(string2, 3)))
+
+        string1, string2 = stage3_helper.get_attribute_from_jsons(jsons_label[0], jsons_label[1], product_segment)
+        feature.append(simfunctions.jaccard(tokenizers.qgram(string1, 3), tokenizers.qgram(string2, 3)))
+
+        features.append(feature)
+        labels.append(stage3_helper.get_01_from_label(jsons_label[2]))
+
+    return features, labels
 
 generate_feature()
--- stage3/stage3_helper.py
+++ stage3/stage3_helper.py
@@ -19,7 +19,7 @@ def read_file(file_name):
     return lines
 
 
-def read_json_label_from_line(line):
+def read_jsons_label_from_line(line):
     line_split = line.split('?')
 
     json1 = json.loads(line_split[2])   # json for product 1
@@ -31,3 +31,37 @@ def read_json_label_from_line(line):
         print 'Error read_json_label_from_line'
 
     return json1, json2, label
+
+
+def get_attribute_from_jsons(json1, json2, attribute):
+    if attribute in json1:
+        string1 = json1[attribute][0]
+    else:
+        string1 = None
+
+    if attribute in json2:
+        string2 = json2[attribute][0]
+    else:
+        string2 = None
+
+    return string1, string2
+
+
+def get_01_from_label(label):
+    if label == 'MATCH':
+        return 0
+    elif label == 'MISMATCH':
+        return 1
+    else:
+        print 'Error get_01_from_label'
+        return None
+
+
+def get_label_from_01(b):
+    if b == 0:
+        return 'MATCH'
+    elif b == 1:
+        return 'MISMATCH'
+    else:
+        print 'Error get_01_from_label'
+        return None
