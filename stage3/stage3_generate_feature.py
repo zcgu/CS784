332647d841ad2b7be5debe0e7087970484952de3
wuyue <yue10owen@gmail.com>
2016-04-05 12:35:33 -0500

    stage3

--- stage3/stage3_generate_feature.py
+++ stage3/stage3_generate_feature.py
@@ -28,7 +28,9 @@ def generate_feature(file_name):
             feature.append(5)
         feature.append(simfunctions.cosine(stage3_helper.stringToSet(string1),stage3_helper.stringToSet(string2)))
         feature.append(simfunctions.overlap_coefficient(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
-
+        feature.append(simfunctions.monge_elkan(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
+        feature.append(simfunctions.tfidf(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
+      
         string1, string2 = stage3_helper.get_attribute_from_jsons(json1, json2, product_name)
         feature.append(simfunctions.jaccard(tokenizers.qgram(string1, 3), tokenizers.qgram(string2, 3)))
         feature.append(simfunctions.jaro_winkler(string1, string2, prefix_weight=0.1))
@@ -38,6 +40,8 @@ def generate_feature(file_name):
             feature.append(5)
         feature.append(simfunctions.cosine(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
         feature.append(simfunctions.overlap_coefficient(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
+        feature.append(simfunctions.monge_elkan(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
+        feature.append(simfunctions.tfidf(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
 
         string1, string2 = stage3_helper.get_attribute_from_jsons(json1, json2, product_segment)
         feature.append(simfunctions.jaccard(tokenizers.qgram(string1, 3), tokenizers.qgram(string2, 3)))
@@ -48,6 +52,8 @@ def generate_feature(file_name):
             feature.append(5)
         feature.append(simfunctions.cosine(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
         feature.append(simfunctions.overlap_coefficient(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
+        feature.append(simfunctions.monge_elkan(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
+        feature.append(simfunctions.tfidf(stage3_helper.stringToSet(string1), stage3_helper.stringToSet(string2)))
 
         features.append(feature)
         labels.append(stage3_helper.get_01_from_label(label))
